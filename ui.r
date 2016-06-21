de07d259b6dcc7ff3896e90afc471623a8b99713
Annie <wuhandaxue2013@gmail.com>
2016-06-21 16:02:41 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,23 +1,23 @@
-# --------------------------------
+# ================================
 # Define UI for application layout
-# --------------------------------
+# ================================
 shinyUI(
   navbarPage(
-    title = "IDNR_SWG App",
+    title = "Iowa DNR MSIM - SGCN Modeling",
     theme = "bootstrap.blue.css",
     inverse = FALSE,
     tabPanel("Map",
              tags$style(type="text/css",
                         ".shiny-output-error { visibility: hidden; }",
                         ".shiny-output-error:before { visibility: hidden; }"),
-             singleton(tags$head(tags$link(rel="stylesheet", type="text/css", href = "https://cdn.datatables.net/buttons/1.1.2/css/buttons.dataTables.min.css"))),
+             singleton(tags$head(tags$link(rel="stylesheet", type="text/css", 
+                                           href = "https://cdn.datatables.net/buttons/1.1.2/css/buttons.dataTables.min.css"))),
              singleton(tags$head(tags$script(src = "https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"))),
              singleton(tags$head(tags$script(src = "https://cdn.datatables.net/buttons/1.1.2/js/dataTables.buttons.min.js"))),
              singleton(tags$head(tags$script(src = "https://cdn.datatables.net/buttons/1.1.2/js/buttons.flash.min.js"))),
              singleton(tags$head(tags$script(src = "https://cdn.datatables.net/buttons/1.1.2/js/buttons.html5.min.js"))),
              singleton(tags$head(tags$script(src = "https://cdn.datatables.net/buttons/1.1.2/js/buttons.print.min.js"))),
              fluidPage(
-               includeCSS("style.css"),
                fluidRow(
                  column(width = 3, wellPanel(
                    selectInput("kind", "Select Specie Category:", width = "auto",
@@ -32,10 +32,11 @@ shinyUI(
                                selected = "Psi"),
                    selectInput("specie", "Select Specie:", 
                                choices = "All"),
-                   checkboxGroupInput("Boundary", "Boundary to show",
-                                      choices = c(`Landform Region` = "Land", "County",
-                                                  `Wildlife Management Units` = "WMU", `Public Lands` = "Public"),
+                   checkboxGroupInput("Boundary", "Boundary",
+                                      choices = c(`Public Lands` = "Public", "County",
+                                                  `Landform Region` = "Land", `Wildlife Management Units` = "WMU"),
                                       select = "Land", inline = F),
+                   helpText("Check to see the boundary lines."),
                    selectInput("public", "Public land name contains (e.g., Adair WMA)",
                                levels(pp$Name), multiple = TRUE, selectize = TRUE),
                    selectInput("county", "County name contains (e.g., Story)",
@@ -47,8 +48,7 @@ shinyUI(
                  )
                  ),
                  column(width = 9,
-                        br(),
-#                        h2("Introduction", align = "center"),
+                        hr(),
                         p("In an effort to prioritize areas of conservation action for all wildlife in Iowa, 
                           particularly Species of Greatest Conservation Need (SGCN),
                           the Iowa Department of Natural Resources", a("(Iowa DNR)", href = "http://www.iowadnr.gov/"), 
@@ -58,19 +58,19 @@ shinyUI(
                           SGCN using data from the MSIM Program."),
                         p("The following map displays predicted values for the parameter of interest based on the covariate for the
                           respective parameter from the", strong("best model.")),
-                        br(),
+                        hr(),
                         fluidRow(
-                          column(width = 6, offset = 2,
+                          column(width = 9, align = "center",
                                  plotOutput("Map", click = clickOpts(id = "plot_click"),
                                             hover = hoverOpts(id = "plot_hover"),
                                             dblclick = "plot_dblclick",
                                             brush = brushOpts(id = "plot_brush", resetOnNew = TRUE),
-                                            width = width)
+                                            width = width, height = height)
                           )
                         ),
                         helpText("Brush and double-click to zoom in; Double-click to resume.", cex = 1.5),
                         fluidRow(
-                          column(width = 6,
+                          column(width = 9,
                                  conditionalPanel(condition = "input.specie != 'All'",
                                                   strong(textOutput("model_info")))
                           )
@@ -103,16 +103,16 @@ shinyUI(
              checkboxInput("all.est", "Show All", value = FALSE),
              tabsetPanel(
                tabPanel("Model", br(),
-                        h5("The table displays the best model for each species as well as the effect size (.Cov) and confidence 
+                        p("The table displays the best model for each species as well as the effect size (.Cov) and confidence 
                           interval (.CI) for the respective covariate on occupancy (Psi), colonization (Gam), and detection (p) 
                           probabilities."),
-                        h5(strong("Bold"), "text indicates a significant effect (confidence interval does not include zero)."),
-                        br(),
+                        p(strong("Bold"), "text indicates a significant effect (confidence interval does not include zero)."),
+                        hr(),
                         DT::dataTableOutput('table1')),
                tabPanel("Real Parameter", br(),
-                        h5("The table displays the real parameter estimates and confidence intervals for occupancy (Psi),
+                        p("The table displays the real parameter estimates and confidence intervals for occupancy (Psi),
                            colonization (Gam), and detection (p) probabilities obtained from the best model for each species."),
-                        br(),
+                        hr(),
                         DT::dataTableOutput('table2')))
     )
   )
